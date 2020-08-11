6531dcae37ca80bb34bd0e45471c82d352d0a848
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-08-11 14:02:29 -0700

    test implemented

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+// const { help } = require('yargs');
 
 const options = new Input();
-const notes = new Notes();
-console.log(options);
+const notes = new Notes(options);
+
+options.valid() ? notes.execute() : help();
+
+function help() {
+  console.log('Error');
+  process.exit();
+}
+
